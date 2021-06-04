51bd1340777ece08c3be006603db7470ac90666e
sabet <sabet@cstructor.com>
2021-06-03 20:34:32 -0700

    Web Project Session 8

---
+++ WebProject/HelloWorld/Models/GuestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        
+        // Exercise 1 - Models
+        public string Email { get; set; }
+        
+        public bool? WillAttend { get; set; }
+    }
+}
