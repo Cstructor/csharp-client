208f3bc17a99083f0f6aa4b3693ae26d32a88282
sabet <sabet@cstructor.com>
2021-06-10 21:02:19 -0700

    Session 9 Final Unit Test

--- WebProject/HelloWorld/Controllers/HomeController.cs
+++ WebProject/HelloWorld/Controllers/HomeController.cs
@@ -32,5 +32,48 @@ namespace HelloWorld.Controllers
             }
         }
 
+        // Product Action
+
+        public IActionResult Product()
+        {
+            //var myProduct = new Product
+            //{
+            //    ProductId = 1,
+            //    Name = "Kayak",
+            //    Description = "A boat for one person",
+            //    Category = "water-sports",
+            //    Price = 200m,
+            //};
+
+            return View(productRepository.Products.First());
+        }
+
+
+        // Products Action - Collection
+        public IActionResult Products()
+        {
+            //var products = new Product[]
+            //{
+            //   new Product{ ProductId = 1, Name = "First One", Price = 1.11m, ProductCount=0},
+            //   new Product{ ProductId = 2, Name="Second One", Price = 2.22m, ProductCount=1},
+            //   new Product{ ProductId = 3, Name="Third One", Price = 3.33m, ProductCount=10},
+            //   new Product{ ProductId = 4, Name="Fourth One", Price = 4.44m, ProductCount=100},
+            //};
+
+            return View(productRepository.Products);
+        }
+
+
+        //====DI===========
+
+        private IProductRepository productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+
+
     }
 }
